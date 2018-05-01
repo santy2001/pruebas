09653a320fc04afa6faa61816f1291205a4009fe
Santiago Gutierrez Ordoñez <sgo_2001@hotmail.com>
2018-05-01 15:09:16 -0500

    Initial Commit

---
+++ PrimerProyecto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PrimerProyecto
+//
+//  Created by SANTIAGO GUTIERREZ on 1/05/18.
+//  Copyright © 2018 SANTIAGO GUTIERREZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
